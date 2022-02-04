e0e6e0bffc010af76e0b214c3bd50f6e4cd88be9
Pavel <prostobux@yandex.ru>
2022-02-04 11:16:57 +0100

    tk menu in progress

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,7 @@
 import csv
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 
 __author__ = "7775152, Usatenko"
 
@@ -47,15 +50,79 @@ def printMenu(menu):
             dishType = dish.type
         print(str(dish.dishId)+")", dish.name, "("+ dish.category +")", "-", dish.price)
 
+def openNewWindow():
+    # Toplevel object which will
+    # be treated as a new window
+    ws = tk.Toplevel()
+
+    game_frame = Frame(ws)
+    game_frame.pack()
+
+    my_game = ttk.Treeview(game_frame)
+
+    my_game['columns'] = ('Id', 'Name', 'Type', 'Category', 'Price')
+
+    my_game.column("#0", width=0,  stretch=NO)
+    my_game.column("Id",anchor=CENTER, width=80)
+    my_game.column("Name",anchor=CENTER,width=80)
+    my_game.column("Type",anchor=CENTER,width=80)
+    my_game.column("Category",anchor=CENTER,width=80)
+    my_game.column("Price",anchor=CENTER,width=80)
+
+    my_game.heading("#0",text="",anchor=CENTER)
+    my_game.heading("Id",text="Id",anchor=CENTER)
+    my_game.heading("Name",text="Name",anchor=CENTER)
+    my_game.heading("Type",text="Type",anchor=CENTER)
+    my_game.heading("Category",text="Category",anchor=CENTER)
+    my_game.heading("Price",text="Price",anchor=CENTER)
+
+
+    
+    menu = createMenu('food.csv') # main list of all dishes
+    for dish in menu:
+        my_game.insert(parent='',index='end',iid=dish.dishId,text='',
+        values=(dish.dishId,dish.name,dish.type, dish.category, dish.price))
+
+
+    my_game.pack()
+
+
+
 # Define all dishes
-menu = createMenu('food.csv') # main list of all dishes
+#menu = createMenu('food.csv') # main list of all dishes
 
 
 
 #TEST SECTION#
 
 table1 = Table(1, 2)
-printMenu(menu)
+#printMenu(menu)
 order1 = table1.createOrder(0, ["soup", "pasta", "vine"])
-print(order1.id)
+#print(order1.id)
+root = tk.Tk()
+root.geometry('600x400+50+50')
+root.configure(background='#F7ECDE')
+greeting = tk.Label(
+    text="Welcome to simple_restaurant!",
+    fg="#54BAB9",
+    bg="#F7ECDE",
+)
+main_text = tk.Label(
+    text="We are happy to see you! Take a look at our Menu!",
+    fg="#54BAB9",
+    bg="#F7ECDE",
+)
+button = tk.Button(
+    text="Menu",
+    width=25,
+    height=5,
+    bg="#E9DAC1",
+    fg="#54BAB9",
+    command = openNewWindow
+)
+
+greeting.pack()
+main_text.pack(pady = 10)
+button.pack(pady = 10)
+root.mainloop()
 
